2d3e16cdc6b915d433b6b18ff597d0ffbd4cae29
dborowicz16 <79418706+dborowicz16@users.noreply.github.com>
2021-10-31 16:07:35 -0400

    Update note.js

--- note.js
+++ note.js
@@ -24,27 +24,28 @@ const Note = mongoose.model('Note', noteSchema);
 
 // Create the function for getting ALL the notes.
 // export it so we can use it in app.js.
-exports.getAll = async function(req, res) {
+exports.getAll = async function (req, res) {
 	const notes = await Note.find({
+
 	});
 	res.json(notes);
 }
 
 //create the deleteOne function
 //export it so we can use it in app.js
-exports.deleteOne = async function(req, res){
-	console.log("Found the ID of " + req.params.useId);
+exports.deleteOne = async function (req, res) {
+	console.log("Found the ID of " + req.params.userId);
 	try {
-		let note = await Note.deleteOne({_id: req.params.userId });
-		// console.log(note);
-		if(!note){
-			console.log("No note was returned");
-		}
-		else {
-			return res.sendStatus(200);
-		}
+		var note = await Note.deleteOne({ _id: req.params.userId }, function (err) {
+			if (!note) {
+				console.log("No note was returned");
+			}
+			else {
+				return res.sendStatus(404);
+			}
+		});
 	}
-	catch (err){
+	catch (err) {
 		console.log(err);
 		res.sendStatus(500);
 	}
@@ -52,59 +53,47 @@ exports.deleteOne = async function(req, res){
 
 //create the putOne function
 //export it so we can use it in app.js
-exports.putOne = async function(req, res){
-	try {
-		const note = new Note({
-			subject: req.body.subject,
-			course: req.body.course,
-			note: req.body.note
-		})
-		var noteId = await Note.putOne({_id: req.params.userId}, function (err){
-			if(!noteId){
-				console.log("No note was found");
-			}
-			else (note.save());
-		})
-	}
-	catch{
-
-	}
+exports.putOne = async function (req, res) {
+	console.log("Found the ID ")
 }
+
 
 //create the putOne function
 //export it so we can use it in app.js
-exports.updateOne = async function(req, res){
-	const note = new Note({
-		subject: req.body.subject,
-		course: req.body.course,
-		note: req.body.note
-	});
-	let error = note.validateSync();
-	if(error){
-		res.sendStatus(400);
-		console.log(error);
-		return;
+exports.updateMany = async function (req, res) {
+	console.log("Replaced the field with the ID " + req.params.userID + "to ")
+	try {
+		var note = await Note.updateMany({ _id: req.params.userID }, { note: 'test' }, function (err, res) {
+			if (!note) {
+				console.log("No note was returned");
+			}
+			else {
+				return res.sendStatus(404);
+			}
+		});
 	}
-	note.save();
-	res.sendStatus(200);
-	return;
+	catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
+
 }
 
 // Create the getOne function.
 // export it so we can use it in app.js.
-exports.getOne = async function(req, res){
+exports.getOne = async function (req, res) {
 	// The getOne function searches the note field in
 	// all of our notes for a search term.  It returns
 	// all the notes that match.
 	// We are using a regex to search.
-	const notes = await Note.find({ 
-		note: { 
-			$regex: req.params.searchTerm 
+	const notes = await Note.find({
+		note: {
+			$regex: req.params.searchTerm
 		}
 	});
-	
+
 	// If there are none that match, send a 404.
-	if(notes.length == 0){
+	if (notes.length == 0) {
 		res.sendStatus(404);
 		return;
 	}
@@ -115,7 +104,7 @@ exports.getOne = async function(req, res){
 
 // Create the function for creating a new note.
 // export is so we can use it in app.js.
-exports.postOne = async function(req, res){
+exports.postOne = async function (req, res) {
 	const note = new Note({
 		subject: req.body.subject,
 		course: req.body.course,
@@ -123,7 +112,7 @@ exports.postOne = async function(req, res){
 	});
 
 	let error = note.validateSync();
-	if(error){
+	if (error) {
 		res.sendStatus(400);
 		console.log(error);
 		return;
